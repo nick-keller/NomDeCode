61f670b169889e827ae4e7adfdda89b16c91aa67
nick-keller <solix@hotmail.fr>
2015-01-29 14:55:13 +0100

    Added color to User

--- src/NDC/UserBundle/Entity/User.php
+++ src/NDC/UserBundle/Entity/User.php
@@ -9,9 +9,51 @@ use FOS\UserBundle\Model\User as BaseUser;
  */
 class User extends BaseUser
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    private $color;
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     * @return User
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
     }
 }
