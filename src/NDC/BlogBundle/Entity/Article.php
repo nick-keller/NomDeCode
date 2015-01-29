849f1ff3333691894312671b91ce5b2732854b04
nick-keller <solix@hotmail.fr>
2015-01-29 11:37:40 +0100

    Updated Article entity

--- src/NDC/BlogBundle/Entity/Article.php
+++ src/NDC/BlogBundle/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace NDC\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use NDC\UserBundle\Entity\User;
 
 /**
  * Article
@@ -19,6 +20,20 @@ class Article
      */
     private $title;
 
+    /**
+     * @var User
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * Get id
@@ -49,6 +64,75 @@ class Article
     public function setTitle($title)
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return \NDC\UserBundle\Entity\User
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set author
+     *
+     * @param \NDC\UserBundle\Entity\User $author
+     * @return Article
+     */
+    public function setAuthor(User $author = null)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Article
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Article
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
