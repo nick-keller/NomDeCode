f8a662f4bd8fbc69e45c2480509f5c859226d0ff
nick-keller <solix@hotmail.fr>
2015-01-29 17:12:15 +0100

    Added Bootstrap and created ArticleAdmin controller

---
+++ src/NDC/BlogBundle/Controller/ArticleAdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace NDC\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManager;
+use Knp\Component\Pager\Paginator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ArticleAdminController extends Controller
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var Paginator
+     */
+    private $paginator;
+
+    /**
+     * @Template
+     */
+    public function indexAction()
+    {
+        return array(
+        );
+    }
+} 
