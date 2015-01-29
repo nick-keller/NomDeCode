6757ea932b9a5062b469f6d2cd5d452e6dbca985
nick-keller <solix@hotmail.fr>
2015-01-29 10:52:23 +0100

    Created Article entity

---
+++ src/NDC/BlogBundle/Entity/Article.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NDC\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ */
+class Article
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
