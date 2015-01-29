e5ef8a624c2ee569ac76e77c028220ad4ac6eb45
nick-keller <solix@hotmail.fr>
2015-01-29 21:56:52 +0100

    Created add action

--- src/NDC/BlogBundle/Controller/ArticleAdminController.php
+++ src/NDC/BlogBundle/Controller/ArticleAdminController.php
@@ -3,10 +3,13 @@
 
 namespace NDC\BlogBundle\Controller;
 
+use NDC\BlogBundle\Entity\Article;
+use NDC\BlogBundle\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\EntityManager;
 use Knp\Component\Pager\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleAdminController extends Controller
 {
@@ -26,6 +29,26 @@ class ArticleAdminController extends Controller
     public function indexAction()
     {
         return array(
+        );
+    }
+
+    /**
+     * @Template
+     */
+    public function addAction()
+    {
+        $article = new Article;
+        $article->setAuthor($this->getUser());
+
+        return $this->handleForm($article);
+    }
+
+    private function handleForm(Article $article, Request $request = null)
+    {
+        $form = $this->createForm(new ArticleType, $article);
+
+        return array(
+            'form' => $form->createView(),
         );
     }
 } 
