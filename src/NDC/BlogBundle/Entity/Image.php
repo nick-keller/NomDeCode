cb5cfa5a70cff03706b89b83fd55561acdc0b769
nick-keller <solix@hotmail.fr>
2015-01-31 10:52:24 +0100

    Created Image entity

---
+++ src/NDC/BlogBundle/Entity/Image.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace NDC\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ */
+class Image
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    private $file;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Image
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
