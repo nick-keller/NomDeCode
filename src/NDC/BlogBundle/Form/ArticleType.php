55e5787c52e19181d81ed3fea7c94fb663838f9b
nick-keller <solix@hotmail.fr>
2015-02-10 09:34:37 +0100

    Changed Article TEch relationship

--- src/NDC/BlogBundle/Form/ArticleType.php
+++ src/NDC/BlogBundle/Form/ArticleType.php
@@ -22,11 +22,9 @@ class ArticleType extends AbstractType
                 'label' => 'Auteur',
                 'class' => 'NDC\UserBundle\Entity\User',
             ))
-            ->add('techs', 'entity', array(
-                'label' => 'Technos',
+            ->add('tech', 'entity', array(
+                'label' => 'Techno',
                 'class' => 'NDC\BlogBundle\Entity\Tech',
-                'multiple' => true,
-                'expanded' => true,
             ))
             ->add('createdAt', 'datetime', array(
                 'label' => 'Créé le',
