265b3e5c4afd734771714021eda37686724c8b8d
nick-keller <solix@hotmail.fr>
2015-02-10 08:18:14 +0100

    updated

--- src/NDC/UserBundle/Repository/UserRepository.php
+++ src/NDC/UserBundle/Repository/UserRepository.php
@@ -12,4 +12,8 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
+    public function queryAll()
+    {
+        return $this->createQueryBuilder('u');
+    }
 }
