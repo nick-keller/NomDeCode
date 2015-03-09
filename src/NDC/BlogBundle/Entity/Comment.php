fd4529c8daaf8199035d77fdb0aab67ee94a91e9
nick-keller <solix@hotmail.fr>
2015-03-09 15:45:09 +0100

    Added comments

---
+++ src/NDC/BlogBundle/Entity/Comment.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace NDC\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ */
+class Comment
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+    /**
+     * @var \NDC\BlogBundle\Entity\Article
+     */
+    private $article;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Comment
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Comment
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getGravatar()
+    {
+        return md5(strtolower(trim($this->email)));
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Comment
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Comment
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get article
+     *
+     * @return \NDC\BlogBundle\Entity\Article
+     */
+    public function getArticle()
+    {
+        return $this->article;
+    }
+
+    /**
+     * Set article
+     *
+     * @param \NDC\BlogBundle\Entity\Article $article
+     * @return Comment
+     */
+    public function setArticle(\NDC\BlogBundle\Entity\Article $article = null)
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+}
---
+++ src/NDC/BlogBundle/Form/CommentType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NDC\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', 'text', array(
+                'label' => 'Pseudo'
+            ))
+            ->add('email', 'text', array(
+                'label' => 'Email'
+            ))
+            ->add('message', 'text', array(
+                'label' => 'Message'
+            ))
+            ->add('Envoyer', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'NDC\BlogBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ndc_blogbundle_comment';
+    }
+}
