117a2be02a488cbddd5bc8600331e0d7d060218b
nick-keller <solix@hotmail.fr>
2015-03-10 13:52:24 +0100

    Finished commentify

--- src/NDC/BlogBundle/Twig/CommentExtension.php
+++ src/NDC/BlogBundle/Twig/CommentExtension.php
@@ -11,12 +11,17 @@ class CommentExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('commentify', array($this, 'commentifyFilter', array('is_safe' => array('html')))),
+            new \Twig_SimpleFilter('commentify', array($this, 'commentifyFilter'), array('pre_escape' => 'html', 'is_safe' => array('html'))),
         );
     }
 
     public function commentifyFilter($txt)
     {
+        $txt = '<p>' . trim($txt) . '</p>';
+        $txt = str_replace("\r", '', $txt);
+        $txt = preg_replace("/\n{2,}/", "</p><p>", $txt);
+        $txt = preg_replace("/\n/", "<br>", $txt);
+        $txt = preg_replace('/https?:\/\/[a-zA-Z0-9\.\-\/\?\=\#\&\~\|\_\@\+\,\$\%\:\;]+/', '<a href="$0" traget="_blank">$0</a>', $txt);
         return $txt;
     }
 
