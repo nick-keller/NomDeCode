f7ac74fb2f52b2d825f9ac4ffb00af4835eca310
Jean-Elie Barjonet <je.barjonet@gmail.com>
2015-03-12 11:38:19 +0100

    stripHTML twig extension

---
+++ src/NDC/BlogBundle/Twig/articlePreviewExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace NDC\BlogBundle\Twig;
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+
+class ArticlePreviewExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('stripHTML', array($this, 'stripHtml'), array('is_safe' => array('html'))),
+        );
+    }
+
+    public function stripHtml($txt, $length = 0)
+    {
+        $txt = strip_tags(trim($txt));
+
+        if($length != 0 && strlen($txt) > $length && strpos($txt, ' ', $length) !== false)
+            $txt = substr($txt, 0, strpos($txt, ' ', $length)).'...';
+
+        return $txt;
+    }
+
+    public function getName()
+    {
+        return 'article_preview_extension';
+    }
+} 
